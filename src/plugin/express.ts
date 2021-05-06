f2eea0cad84994655f43a1c4bc251f5571408bca
yuta ohira <alesion90@gmail.com>
2021-05-06 17:33:40 +0900

    CORS

--- src/plugin/express.ts
+++ src/plugin/express.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import { Request, Response, NextFunction } from "@/typings/express";
 const app: express.Express = express();
+
+// CORS
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 export default app;
--- src/typings/express.ts
+++ src/typings/express.ts
@@ -1,3 +1,4 @@
 import express from "express";
 export type Request = express.Request;
 export type Response = express.Response;
+export type NextFunction = express.NextFunction;
